bbc6f4b84b928e8de1c2f1d8a62a4a4f57e21fc8
titty91 <tznbaraldi@gmail.com>
2021-04-26 18:31:45 +0200

    fine heder inizio jambo

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,8 @@
 let mix = require('laravel-mix');mix
     .js('src/app.js', 'dist')
     .sass('src/scss/style.scss', '')
+    .options({
+      processCssUrls:false
+    })
     .setPublicPath('dist')
     ;
